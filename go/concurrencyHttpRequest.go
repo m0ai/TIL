6d93eacb07e3047727c02423cf37a07a022539c6
m0ai <frozenmoai@gmail.com>
2021-01-22 01:43:20 +0900

    Simle curl

---
+++ go/concurrencyHttpRequest.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type requestResult struct {
+	url string
+	status string
+}
+
+func main() {
+	results := make(map[string]string)
+	c := make(chan requestResult)
+	urls := []string {
+		"https://google.com",
+		"https://airbnb.com",
+	}
+
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+
+	for i := 0; i < len(urls); i ++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+}
+
+func hitURL(url string, c chan<- requestResult) (status string) {
+	resp, err := http.Get(url)
+	status = "OK"
+	if err != nil || resp.StatusCode >= http.StatusBadRequest {
+		status = "FAILED"
+	}
+	c <- requestResult{url:url, status: status}
+	return
+}
